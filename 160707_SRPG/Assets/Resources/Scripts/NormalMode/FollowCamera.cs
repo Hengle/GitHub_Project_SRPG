fc6e1c92e9505ff2d3454dfcf3844b5475e1176e
_lolol <ninjapanda0@naver.com>
2016-10-13 17:55:09 +0900

    161013 update

---
+++ 160707_SRPG/Assets/Resources/Scripts/NormalMode/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = target.position;   // 바로 어께 위에서 따라 감 
+        transform.position = target.position + offset;
+    }
+}
--- 160707_SRPG/Assets/Resources/Scripts/NormalMode/UserControl.cs
+++ 160707_SRPG/Assets/Resources/Scripts/NormalMode/UserControl.cs
@@ -4,25 +4,40 @@ using System.Collections;
 public class UserControl : MonoBehaviour
 {
     Animator animator;
-
+    Vector3 lookDirection;
+    float turnSpeed = 3f;
+    
     // Use this for initialization
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GameObject.Find("UserPlayer").GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        Debug.Log("V : " + v);
-        Debug.Log("H : " + h);
-        animator.SetFloat("user_walk_float", v);
-        animator.SetFloat("user_walk_float", h);
-
-        transform.Translate(Vector3.forward * v * Time.deltaTime);  // 전/후
-        transform.Translate(Vector3.right * h * Time.deltaTime);    // 좌/우
+        float x = Input.GetAxisRaw("Vertical");     // 전,후
+        float z = Input.GetAxisRaw("Horizontal");   // 좌,우
+        
+        if (x != 0 || z != 0)
+        {
+            animator.SetBool("user_walk2", true);
+        }
+        else
+        {
+            animator.SetBool("user_walk2", false);
+        }
+        
+        // if문으로 방향이 0일때는 회전하지 않게함.
+        if (!(x == 0 && z == 0))
+        {
+            // 바라보는 방향
+            lookDirection = x * Vector3.forward + z * Vector3.right;
+            // 회전
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(lookDirection),
+                Time.fixedDeltaTime * turnSpeed);
+            // 이동
+            transform.Translate(Vector3.forward * Time.fixedDeltaTime);
+        }
     }
 }
