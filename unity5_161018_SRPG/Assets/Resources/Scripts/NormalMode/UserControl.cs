701951ae507658f70026306c1b86452d1deecc68
_lolol <ninjapanda0@naver.com>
2016-10-18 17:46:27 +0900

    161018 update

--- unity5_161018_SRPG/Assets/Resources/Scripts/NormalMode/UserControl.cs
+++ unity5_161018_SRPG/Assets/Resources/Scripts/NormalMode/UserControl.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UserControl : MonoBehaviour
 {
     Animator animator;
     Vector3 lookDirection;
     float turnSpeed = 3f;
-    float x;
-    float z;
+
+    public Image fade;
+    float fades = 0.0f;
+    float time = 0f;
 
     // Use this for initialization
     void Start()
@@ -19,8 +22,8 @@ public class UserControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxisRaw("Vertical");     // 전,후
-        z = Input.GetAxisRaw("Horizontal");   // 좌,우
+        float x = Input.GetAxisRaw("Vertical");     // 전,후
+        float z = Input.GetAxisRaw("Horizontal");   // 좌,우
         
         if (x != 0 || z != 0)
         {
@@ -50,14 +53,25 @@ public class UserControl : MonoBehaviour
         {
             Debug.Log("부딪혔다~~");
             animator.SetTrigger("user_attack");
+
             StartCoroutine(SceneChanges());
         }
     }
 
     IEnumerator SceneChanges()
     {
-        // 3초 대기 후 아랫단 실행
-        yield return new WaitForSeconds(2f);
-        SceneManager.LoadScene(1);
+        time += Time.deltaTime;
+        if (time >= 0.1f)
+        {
+            fades += 0.1f;
+            fade.color = new Color(0f, 0f, 0f, fades);
+        }
+        else if (fades >= 1.0f)
+        {
+            // 3초 대기 후 아랫단 실행
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene(1);
+            time = 0f;
+        }
     }
 }
