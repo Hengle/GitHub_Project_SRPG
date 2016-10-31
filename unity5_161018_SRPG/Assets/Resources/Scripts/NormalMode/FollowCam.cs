66a7e0745cd093026a7ae98de1129c8a6b57c60a
_lolol <ninjapanda0@naver.com>
2016-10-31 11:17:50 +0900

    161031 1rst update

--- unity5_161018_SRPG/Assets/Resources/Scripts/NormalMode/FollowCam.cs
+++ unity5_161018_SRPG/Assets/Resources/Scripts/NormalMode/FollowCam.cs
@@ -8,30 +8,37 @@ public class FollowCam : MonoBehaviour
 
     private float x = 0.0f;
     private float y = 0.0f;
+    // 화면 회전 속도(마우스 기준)
     private int mouseXSpeedmod = 5;
     private int mouseYSpeedmod = 3;
-
     // 최대 줌아웃 크기
     public float maxViewDistance = 6;
     // 최소 줌인 크기
     public float minViewDistance = 1;
     // 카메라 줌 속도
     public int zoomRate = 30;
-    // 카메라 회전 속도
+    // 카메라 회전 속도(캐릭터 기준)
     private int lerpRate = 5;
-    // 플레이어와의 거리(카메라 현재위치=5만큼 떨어지게 설정)
-    private float desiredDistance = 5f;
-    // 수정?된 거리..
+    // 거리 초기값(카메라 현재위치=플레이어와 5만큼 떨어지게 설정)
+    private float distance = 5f;
+    // 플레이어와의 거리 계산용
+    private float desiredDistance;
+    // 수정?된 거리
     private float correctedDistance;
-    // 
+    private float currentDistance;
+    // 플레이어의 
     public float cameraTargetHeight = 1.0f;
-
+    
     // Use this for initialization
     void Start ()
     {
         // 카메라 각도 초기화
         Vector3 angles = transform.eulerAngles;
         y = angles.y + 60f;
+        // 카메라와 플레이어간의 거리 초기화
+        currentDistance = distance;
+        desiredDistance = distance;
+        correctedDistance = distance;
     }
 	
 	void LateUpdate ()
@@ -46,6 +53,7 @@ public class FollowCam : MonoBehaviour
         }
         else if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
         {
+            // 카메라 = 캐릭터가 바라보는 방향으로 따라다님
             float targetRotationAngle = cameraTarget.eulerAngles.y;
             float cameraRotationAngle = transform.eulerAngles.y;
             x = Mathf.LerpAngle(cameraRotationAngle, targetRotationAngle, lerpRate * Time.deltaTime);
@@ -53,29 +61,39 @@ public class FollowCam : MonoBehaviour
 
         // 위,아래 방향 회전 최대/최소각
         y = ClampAngle(y, -50, 80);
-        // 회전 값 저장
+        // 카메라 (상하좌우) 회전 값 저장
         Quaternion rotations = Quaternion.Euler(y, x, 0);
 
-        // 마우스 휠:카메라 줌인/아웃 거리 구함
+        // 카메라 줌 인/아웃 거리 구함(마우스 휠)
         desiredDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance);
         desiredDistance = Mathf.Clamp(desiredDistance, minViewDistance, maxViewDistance);
         correctedDistance = desiredDistance;
         // 거리(위치) 값 저장
         Vector3 positions = cameraTarget.position - (rotations * Vector3.forward * desiredDistance);
 
-        //RaycastHit collisionHit;
-        //Vector3 cameraTargetPosition = new Vector3(cameraTarget.position.x, cameraTarget.position.y + cameraTargetHeight, cameraTarget.position.z);
 
-        //bool isCorrected = false;
-        //if(Physics.Linecast(cameraTargetPosition, positions, out collisionHit))
-        //{
-        //    positions = collisionHit.point;
-        //    correctedDistance = Vector3.Distance(cameraTargetPosition, positions);
-        //}
+        // ==============코드해석해야됨============================
+        RaycastHit collisionHit;
+        Vector3 cameraTargetPosition = new Vector3(cameraTarget.position.x, cameraTarget.position.y + cameraTargetHeight, cameraTarget.position.z);
 
-        // main camera의 rotations 적용
+        bool isCorrected = false;
+        if (Physics.Linecast(cameraTargetPosition, positions, out collisionHit))
+        {
+            positions = collisionHit.point;
+            correctedDistance = Vector3.Distance(cameraTargetPosition, positions);
+            isCorrected = true;
+        }
+
+        // condition ? first_expression : second_expression
+        currentDistance = !isCorrected || correctedDistance > currentDistance ? Mathf.Lerp(currentDistance, correctedDistance, Time.deltaTime * zoomRate) : correctedDistance;
+
+        positions = cameraTarget.position - (rotations * Vector3.forward * currentDistance + new Vector3());
+        // ========================================================
+        positions.y += 1f;
+
+        // Main Camera의 rotations 적용
         transform.rotation = rotations;
-        // main camera의 potitions 적용
+        // Main Camera의 potitions 적용
         transform.position = positions;
     }
 
